1a0ddee13c20075b86374adf96fd78ccc9d2833f
ChangJoEconomy <47021837+ChangJoEconomy@users.noreply.github.com>
2023-05-19 20:36:33 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 // geodesy 라이브러리 로드
-import Mgrs, { LatLon } from './geodesy/mgrs.js';
+import Mgrs, { LatLon } from 'geodesy/mgrs.js';
 
 function convertMGRSToLatLng(mgrs) {
   var latlon = LatLon.parseMGRS(mgrs);
